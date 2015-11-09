c3b67bea6092e9499cf9018ffafea73e396e0cb2
Peter Verkade <peter.m.verkade@gmail.com>
2015-11-09 12:33:35 +0100

    MoonMan - NotoriousKKK

--- week2/euler.py
+++ week2/euler.py
@@ -7,11 +7,8 @@ def euler(y0, t0, t1, f, stepsize):
     t = t0
 
     while t < t1:
-        try:
-            y += stepsize * f(t, y)
-            t += stepsize
-        except Exception:
-            return None
+        y += stepsize * f(t, y)
+        t += stepsize
        
     if not np.isfinite(y):
         return None
@@ -25,7 +22,6 @@ def rungekutta2(y0, t0, t1, f, stepsize):
     t = t0
 
     while t < t1:
-    
         An = f(t, y)
         y_approx = y + stepsize * An
         Bn = f(t + stepsize, y_approx)
@@ -44,7 +40,7 @@ def rungekutta4(y0, t0, t1, f, stepsize):
     y = y0
     t = t0
 
-    while t < t1:
+    while t < t1:   
         k1 = f(t, y)
         y1 = y + 0.5 * k1 * stepsize
         
@@ -67,12 +63,38 @@ def rungekutta4(y0, t0, t1, f, stepsize):
     
 if __name__ == "__main__":
     
+    
     f = lambda t, y: 1
+    print "--++{ dy/dt = 1 }++--"
     print euler(0, 0, 10, f, 0.125)
     print rungekutta2(0, 0, 10, f, 0.125)
     print rungekutta4(0, 0, 10, f, 0.125)
-    print rungekutta4(0, 0, 10, f, 0.25)
     
+    f = lambda t, y: t
+    print "--++{ dy/dt = t }++--"
+    print euler(0, 0, 10, f, 0.125)
+    print rungekutta2(0, 0, 10, f, 0.125)
+    print rungekutta4(0, 0, 10, f, 0.125)
+    
+    f = lambda t, y: y
+    print "--++{ dy/dt = y }++--"
+    print euler(1, 0, 5, f, 0.125)
+    print rungekutta2(1, 0, 5, f, 0.125)
+    print rungekutta4(1, 0, 5, f, 0.125)
+    
+    f = lambda t, y: y * y
+    print "--++{ dy/dt = y ** 2 }++--"
+    print euler(1, -1, 1, f, 0.125)
+    print rungekutta2(1, -1, 1, f, 0.125)
+    print rungekutta4(1, -1, 1, f, 0.125)
+    
+    f = lambda t, y: y * y
+    print "--++{ dy/dt = y ** 2 }++--"
+    print euler(1, 1, 10, f, 0.125)
+    print rungekutta2(1, 1, 10, f, 0.125)
+    print rungekutta4(1, 1, 10, f, 0.125)
+    
+    """
     f = lambda t, y : y + 2
     results = [euler(2, 0, t, f, 0.125) for t in np.arange(0, 10.1, 0.1)]
     plt.plot(np.arange(0, 10.1, 0.1), results)
@@ -80,5 +102,5 @@ if __name__ == "__main__":
     ax = plt.gca()
     ax.set_yscale("log")
     plt.show()
-    
+    """
     
