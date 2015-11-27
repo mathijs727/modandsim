2d5f8beda55834fab221fbbe61775ffa4ec69145
Mathijs <mathijs.l.molenaar@gmail.com>
2015-11-27 17:48:43 +0100

    Should work on linux/os x now

--- week3/boltzmann/boltzmann.cpp
+++ week3/boltzmann/boltzmann.cpp
@@ -8,7 +8,13 @@
 
 #include <chrono>
 #include <iostream>
-#include <Windows.h>
+#ifdef WIN32
+#include <windows.h>
+#elif _POSIX_C_SOURCE >= 199309L
+#include <time.h>   // for nanosleep
+#else
+#include <unistd.h> // for usleep
+#endif
 
 void setupOpenGL()
 {
@@ -90,7 +96,17 @@ int main(int argc, char** argv)
 	FPSCounter fpsCounter = FPSCounter();
 	while (!window.shouldClose())
 	{
-		Sleep(100);
+		int milliseconds = 80;
+#ifdef WIN32
+		Sleep(milliseconds);
+#elif _POSIX_C_SOURCE >= 199309L
+		struct timespec ts;
+    	ts.tv_sec = milliseconds / 1000;
+    	ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    	nanosleep(&ts, NULL);
+#else
+		usleep(milliseconds * 1000);
+#endif
 		Window::pollEvents();
 		fpsCounter.update();
 
